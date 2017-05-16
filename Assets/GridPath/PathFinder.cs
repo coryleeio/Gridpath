df5fd1024fa67d389842576c484bddb5d6b03821
Cory Lee <cl10001123@gmail.com>
2017-05-17 01:21:46 +0900

    Cleanup

---
+++ Assets/GridPath/PathFinder.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+namespace GridPath
+{
+    public class PathFinder : MonoBehaviour
+    {
+        public int gridSizeX;
+        public int gridSizeY;
+        private GridGraph _grid;
+        public GridGraph Grid
+        {
+            get
+            {
+                return _grid;
+            }
+            set
+            {
+                _grid = value;
+            }
+        }
+        
+        void Start()
+        {
+            GridGraph grid = new GridGraph(gridSizeX, gridSizeY);
+            PathSolver solver = new PathSolver();
+            var path = solver.FindPath(new Point(0, 0), new Point(3, 3), grid);
+        }
+    }
+}
